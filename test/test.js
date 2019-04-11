c295f4337f5fd3314e411b982331c7672444458a
J.R. Strayhorn <jrstrayhorn@gmail.com>
2019-04-11 14:16:06 -0400

    done with example

--- test/test.js
+++ test/test.js
@@ -1,8 +1,26 @@
 var expect = require('chai').expect;
+var assert = require('assert');
+
+describe('Basic Mocha String Test', () => {
+  it('should return number of characters in a string', () => assert.equal('Hello'.length, 5));
+  it('should return first character of the string', () => assert.equal('Hello'.charAt(0), 'H'));
+});
 
 class SimpleCalc {
-  add(a, b) {
-    return a + b;
+  constructor(value) {
+    this._startingPoint = value || 0;
+  }
+
+  add(value) {
+    return new SimpleCalc(this._startingPoint + value);
+  }
+
+  get result() {
+    return this._startingPoint;
+  }
+
+  subtract(value) {
+    return new SimpleCalc(this._startingPoint - value);
   }
 }
 
@@ -30,13 +48,50 @@ describe('Simple Calculator', () => {
 
     it('adds two numbers', () => {
       // arrange
-      let calc = new SimpleCalc();
+      let calc = new SimpleCalc(1);
 
       // act
-      let result = calc.add(1, 2);
+      let result = calc.add(2).result;
 
       // assert
       expect(result).to.equal(3);
+    });
+  });
+
+  describe('subtract function', () => {
+    it('exists', () => {
+      // arrange
+      let calc;
+
+      // act
+      calc = new SimpleCalc();
+
+      // assert
+      expect(calc.subtract).to.exist;
+    });
+
+    it('subtracts two numbers', () => {
+      // arrange
+      let calc = new SimpleCalc(3);
+
+      // act
+      let result = calc.subtract(2).result;
+
+      // assert
+      expect(result).to.equal(1);
+    });
+  });
+
+  describe('chaining functions', () => {
+    it('adds and subtracts chained', () => {
+      // arrange
+      let calc = new SimpleCalc(5);
+
+      // act
+      let result = calc.add(3).subtract(4).result;
+
+      // assert
+      expect(result).to.equal(4);
     });
   });
 });
